3688fa66bf1b0ab89fa1e951e32bdf1102b545b7
Keith Burdis <keith@burdis.org>
2012-10-29 08:35:39 +0000

    Enable retrieving config from hiera.  Added a users parameter to specify a list of users to set up.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,29 @@
 class runit (
-  $package_file = undef
+  $package_file = undef,
+  $users        = [],
+  $use_hiera    = true
 ) {
   # Only run on RedHat derived systems.
   case $::osfamily {
     RedHat: { }
     default: {
-      fail('This module only supports RedHat-based systems')
+      fail('This module currently only supports RedHat-based systems')
     }
   }
-  class { 'runit::install':
-    package_file => $package_file,
+  if $use_hiera {
+    $runit = hiera_hash('runit')
+    class { 'runit::install':
+      package_file => $runit['package_file'] ? {
+        undef   => $package_file,
+        default => $runit['package_file'],
+      }
+    }
+  }
+  else {
+    class { 'runit::install':
+      package_file => $package_file,
+    }
   }
   include runit::service
+  create_resources( 'runit::user', $users )
 }
