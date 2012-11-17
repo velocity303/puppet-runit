487c8788ba4c4a865ca181cc2858dfdd947e2fae
Keith Burdis <keith@burdis.org>
2012-11-17 21:18:18 +0000

    Create a Runit service for a user with default configuration but not started.  The run script must be replaced with a script to start the service but the default scripts will suffice for most services.

---
+++ manifests/service.pp
@@ -0,0 +1,70 @@
+define runit::service  (
+  $user             = undef,
+  $group            = undef,
+  $description      = undef,
+  $restart_interval = 30,
+  $restart_count    = 3,
+  $clear_interval   = 300,
+  $log_size         = 1000000000,
+  $log_max_files    = 30,
+  $log_min_files    = 2,
+  $log_rotate_time  = 86400,
+  $home             = '/home'
+) {
+  $service = $title
+  file { "${home}/${user}/service/${service}":
+    ensure  => directory,
+    mode    => '0750',
+    owner   => $user,
+    group   => $group,
+  }
+  file { "${home}/${user}/service/${service}/run":
+    ensure  => present,
+    mode    => '0550',
+    owner   => $user,
+    group   => $group,
+    content => template('runit/service/run.erb'),
+    replace => false,
+    require => File["${home}/${user}/service/${service}"],
+    notify  => Exec["do-not-start-${user}-${service}"],
+  }
+  exec { "do-not-start-${user}-${service}":
+    command     => "/usr/bin/touch ${home}/${user}/service/${service}/down",
+    creates     => "${home}/${user}/service/${service}/down",
+    refreshonly => true,
+  }
+  file { "${home}/${user}/service/${service}/finish":
+    ensure  => present,
+    mode    => '0550',
+    owner   => $user,
+    group   => $group,
+    content => template('runit/service/finish.erb'),
+    replace => false,
+    require => File["${home}/${user}/service/${service}"],
+  }
+  file { "${home}/${user}/service/${service}/log":
+    ensure  => directory,
+    mode    => '0750',
+    owner   => $user,
+    group   => $group,
+    require => File["${home}/${user}/service/${service}"],
+  }
+  file { "${home}/${user}/service/${service}/log/run":
+    ensure  => present,
+    mode    => '0550',
+    owner   => $user,
+    group   => $group,
+    content => template('runit/service/log_run.erb'),
+    replace => false,
+    require => File["${home}/${user}/service/${service}/log"],
+  }
+  file { "${home}/${user}/service/${service}/log/config":
+    ensure  => present,
+    mode    => '0440',
+    owner   => $user,
+    group   => $group,
+    content => template('runit/service/log_config.erb'),
+    replace => false,
+    require => File["${home}/${user}/service/${service}/log"],
+  }
+}
