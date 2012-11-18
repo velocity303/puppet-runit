9aee27a6568206de8c803e3c3d0da0e7469e3a20
Keith Burdis <keith@burdis.org>
2012-11-18 20:54:38 +0000

    Create files separate from the service directory to avoid partially started services.

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,59 +11,51 @@ define runit::service  (
   $home             = '/home'
 ) {
   $service = $title
-  file { "${home}/${user}/service/${service}":
+  exec { "${user}-runit-${service}":
+    command => "/bin/mkdir -p ${home}/${user}/runit",
+    creates => "${home}/${user}/runit",
+    user    => $user,
+    group   => $group,
+  }
+  file { "${home}/${user}/runit/${service}":
     ensure  => directory,
     mode    => '0750',
     owner   => $user,
     group   => $group,
+    require => Exec["${user}-runit-${service}"],
   }
-  file { "${home}/${user}/service/${service}/run":
-    ensure  => present,
-    mode    => '0550',
-    owner   => $user,
-    group   => $group,
-    content => template('runit/service/run.erb'),
-    replace => false,
-    require => File["${home}/${user}/service/${service}"],
-    notify  => Exec["do-not-start-${user}-${service}"],
-  }
-  exec { "do-not-start-${user}-${service}":
-    command     => "/bin/touch ${home}/${user}/service/${service}/down",
-    creates     => "${home}/${user}/service/${service}/down",
-    refreshonly => true,
-  }
-  file { "${home}/${user}/service/${service}/finish":
+  file { "${home}/${user}/runit/${service}/finish":
     ensure  => present,
     mode    => '0550',
     owner   => $user,
     group   => $group,
     content => template('runit/service/finish.erb'),
     replace => false,
-    require => File["${home}/${user}/service/${service}"],
+    require => File["${home}/${user}/runit/${service}"],
   }
-  file { "${home}/${user}/service/${service}/log":
+  file { "${home}/${user}/runit/${service}/log":
     ensure  => directory,
     mode    => '0750',
     owner   => $user,
     group   => $group,
-    require => File["${home}/${user}/service/${service}"],
+    require => File["${home}/${user}/runit/${service}"],
   }
-  file { "${home}/${user}/service/${service}/log/run":
+  file { "${home}/${user}/runit/${service}/log/run":
     ensure  => present,
     mode    => '0550',
     owner   => $user,
     group   => $group,
     content => template('runit/service/log_run.erb'),
     replace => false,
-    require => File["${home}/${user}/service/${service}/log"],
+    require => File["${home}/${user}/runit/${service}/log"],
   }
-  file { "${home}/${user}/service/${service}/log/config":
+  file { "${home}/${user}/runit/${service}/log/config":
     ensure  => present,
     mode    => '0440',
     owner   => $user,
     group   => $group,
     content => template('runit/service/log_config.erb'),
     replace => false,
-    require => File["${home}/${user}/service/${service}/log"],
+    require => File["${home}/${user}/runit/${service}/log"],
   }
 }
